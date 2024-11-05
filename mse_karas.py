691c911752756c9e44e3013336d5637d0ccd2198
coderharshith <130730951+coderharshith@users.noreply.github.com>
2024-11-05 10:04:51 +0530

    main file

---
+++ mse_karas.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from  sklearn.metrics import mean_squared_error
+
+print("testing")
